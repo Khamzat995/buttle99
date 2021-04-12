c5d3931e09247f3b3d19a656a4fb90de24c7c085
kuduzovkhamzat@gmail.com <khamzat995@gmail.com>
2021-04-12 11:34:10 +0300

    domashka

--- домашка.js
+++ домашка.js
@@ -5,19 +5,39 @@
 
 
 function cryptText(text) {
-  const glas = ['а','о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я'];
-  const soglas = ['б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ'];
-    for (let i = 0; i < glas.length; i++) {
-      if (text.length >= 3 && glas[i] === text[0]) {
-        return 'код' + text
-      } else if(text.length >= 3) {
-        return 'инту' + text;
-      } return text + 'ик'
+  //const glas = ['а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я'];
+  const words = text.split(' ');
 
-
+  for (let i = 0; i < words.length; i++) {
+    if(words[i] == Number(words[i])) {
+      words[i] = replaceNumber(words[i])
+    } else if(words[i].length < 3) {
+      words[i] = `${words[i]}${'ик'}`;
+    } else {
+      words[i] = replaceByRules(words[i]);
     }
-
+    return words.join(' ');
+  }
 }
 
-console.log(cryptText('пр'))
+// for (let i = 0; i < soglas.length; i++) {
 
+// console.log('lorem ipsum'.split(' '));
+      function replaceNumber(word) {
+        if(word.length < 3) {
+          return word.reverse();
+        } else {
+          return `${word[0]}${word}${word[word.length-1]}`;
+        }
+      }
+
+      function replaceByRules(word) {
+        const vowels = ['а', 'и', 'у', 'о', 'е', 'ы', 'э', 'ю', 'я', 'ё']
+        if(vowels.indexOf(word[0]) !== -1) {
+          return 'код' + word
+        } else {
+          return 'инту' + word
+        }
+      }
+
+console.log(cryptText('я вас обязан известить'))
